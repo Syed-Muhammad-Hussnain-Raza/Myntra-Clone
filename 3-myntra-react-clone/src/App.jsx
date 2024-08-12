e47168727eea964021c45086cf9f9c87c9beb3ff
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-12 13:18:11 +0500

    All the required components are imported here, there will be changes in functionality of some very soon

--- 3-myntra-react-clone/src/App.jsx
+++ 3-myntra-react-clone/src/App.jsx
@@ -1,9 +1,34 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeItem from "./components/HomeItem";
 
 function App() {
+  const item = {
+    id: "001",
+    image: "images/1.jpg",
+    company: "Carlton London",
+    item_name: "Rhodium-Plated CZ Floral Studs",
+    original_price: 1045,
+    current_price: 606,
+    discount_percentage: 42,
+    return_period: 14,
+    delivery_date: "10 Oct 2023",
+    rating: {
+      stars: 4.5,
+      count: 1400,
+    },
+  };
+
   return (
     <>
-      <h1>Myntra React Clone</h1>
+      <Header />
+      <main>
+        <div className="items-container">
+          <HomeItem item={item} />
+        </div>
+      </main>
+      <Footer />
     </>
   );
 }
