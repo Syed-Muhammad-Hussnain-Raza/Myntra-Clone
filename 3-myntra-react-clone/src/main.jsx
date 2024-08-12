bcf87086133ff373554771d4c67d512e2489e930
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-12 15:58:30 +0500

    React-Router concept is defined here to made navigation using links

--- 3-myntra-react-clone/src/main.jsx
+++ 3-myntra-react-clone/src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./routes/App.jsx";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Bag from "./routes/Bag.jsx";
+import Home from "./routes/Home.jsx";
+
+// Defining router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/bag",
+        element: <Bag />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
