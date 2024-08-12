b3e31e8a640e0a965535abb15f8f7dbc99e75e43
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-12 16:47:43 +0500

    BagSummary.jsx - Deals with basic information like Total MRP, Discount, Total amount etc for Items given to it

---
+++ 3-myntra-react-clone/src/components/BagSummary.jsx
@@ -0,0 +1,41 @@
+const BagSummary = () => {
+  const bagSummary = {
+    totalItem: 10,
+    totalMRP: 50,
+    totalDiscount: 100,
+    finalPayment: 400,
+  };
+  return (
+    <div className="bag-summary">
+      <div className="bag-details-container">
+        <div className="price-header">
+          PRICE DETAILS ({bagSummary.totalItem} Items){" "}
+        </div>
+        <div className="price-item">
+          <span className="price-item-tag">Total MRP</span>
+          <span className="price-item-value">₹{bagSummary.totalMRP}</span>
+        </div>
+        <div className="price-item">
+          <span className="price-item-tag">Discount on MRP</span>
+          <span className="price-item-value priceDetail-base-discount">
+            -₹{bagSummary.totalDiscount}
+          </span>
+        </div>
+        <div className="price-item">
+          <span className="price-item-tag">Convenience Fee</span>
+          <span className="price-item-value">₹99</span>
+        </div>
+        <hr />
+        <div className="price-footer">
+          <span className="price-item-tag">Total Amount</span>
+          <span className="price-item-value">₹{bagSummary.finalPayment}</span>
+        </div>
+      </div>
+      <button className="btn-place-order">
+        <div className="css-xjhrni">PLACE ORDER</div>
+      </button>
+    </div>
+  );
+};
+
+export default BagSummary;
