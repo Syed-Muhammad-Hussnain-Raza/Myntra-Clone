bb73fb2d6a6d3a4060459cc03f67ee3a46567d72
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-12 15:43:40 +0500

    Update - Place of Bag.jsx changes from component to routes

---
+++ 3-myntra-react-clone/src/routes/Bag.jsx
@@ -0,0 +1,12 @@
+const Bag = () => {
+  return (
+    <main>
+      <div className="bag-page">
+        <div className="bag-items-container"></div>
+        <div className="bag-summary"></div>
+      </div>
+    </main>
+  );
+};
+
+export default Bag;
