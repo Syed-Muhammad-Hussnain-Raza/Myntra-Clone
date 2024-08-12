60aef18665236b4bc3d98ffee738f6b0a19a953b
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-12 15:55:39 +0500

    This component is updated and all the code is manages in different sub-components

--- 3-myntra-react-clone/src/routes/App.jsx
+++ 3-myntra-react-clone/src/routes/App.jsx
@@ -1,30 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import HomeItem from "../components/HomeItem";
-import Home from "./Home";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const item = {
-    id: "001",
-    image: "images/1.jpg",
-    company: "Carlton London",
-    item_name: "Rhodium-Plated CZ Floral Studs",
-    original_price: 1045,
-    current_price: 606,
-    discount_percentage: 42,
-    return_period: 14,
-    delivery_date: "10 Oct 2023",
-    rating: {
-      stars: 4.5,
-      count: 1400,
-    },
-  };
-
   return (
     <>
       <Header />
-      <Home />
+      <Outlet></Outlet>
       <Footer />
     </>
   );
