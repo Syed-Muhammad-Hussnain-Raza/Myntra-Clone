f4890ed714089f62ac454f73b35fff7b79c9774d
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-12 15:54:17 +0500

    The anchor tag with href attribute is replaced by Link with to attribute so that the link will not update while navigation

--- 3-myntra-react-clone/src/components/Header.jsx
+++ 3-myntra-react-clone/src/components/Header.jsx
@@ -1,17 +1,18 @@
 import { BsFillPersonFill } from "react-icons/bs";
 import { FaFaceGrinHearts, FaBagShopping } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <div className="logo_container">
-        <a href="#">
+        <Link to="/">
           <img
             className="myntra_home"
             src="images/myntra_logo.webp"
             alt="Myntra Home"
           />
-        </a>
+        </Link>
       </div>
       <nav className="nav_bar">
         <a href="#">Men</a>
@@ -41,11 +42,11 @@ const Header = () => {
           <span className="action_name">Wishlist</span>
         </div>
 
-        <a className="action_container" href="pages/bag.html">
+        <Link className="action_container" to="/bag">
           <FaBagShopping />
           <span className="action_name">Bag</span>
           <span className="bag-item-count">0</span>
-        </a>
+        </Link>
       </div>
     </header>
   );
