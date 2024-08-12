ab5b9f23c1b0915b962d24af7f6954eed118b04c
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-13 01:27:57 +0500

    HomeItem.jsx - Functionality of toggle button introduced for add and remove button and also for items in bag add and remove on click respectively

--- 3-myntra-react-clone/src/components/HomeItem.jsx
+++ 3-myntra-react-clone/src/components/HomeItem.jsx
@@ -1,4 +1,21 @@
+import { useDispatch, useSelector } from "react-redux";
+import { bagActions } from "../store/bagSlice";
+import { GrAddCircle } from "react-icons/gr";
+import { AiFillDelete } from "react-icons/ai";
+
 const HomeItem = ({ item }) => {
+  const dispatch = useDispatch();
+  const bagItems = useSelector((store) => store.bag);
+  const elementFound = bagItems.indexOf(item.id) >= 0;
+
+  const handleAddToBag = () => {
+    dispatch(bagActions.addToBag(item.id));
+  };
+
+  const handleRemove = () => {
+    dispatch(bagActions.removeFromBag(item.id));
+  };
+
   return (
     <div className="item-container">
       <img className="item-image" src={item.image} alt="item image" />
@@ -12,12 +29,24 @@ const HomeItem = ({ item }) => {
         <span className="original-price">Rs {item.original_price}</span>
         <span className="discount">({item.discount_percentage}% OFF)</span>
       </div>
-      <button
-        className="btn-add-bag"
-        onClick={() => console.log("Add to bg button clicked")}
-      >
-        Add to Bag
-      </button>
+      {elementFound ? (
+        <button
+          type="button"
+          className="btn btn-add-bag btn-danger"
+          onClick={handleRemove}
+        >
+          <AiFillDelete /> Remove
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="btn btn-add-bag btn-success"
+          onClick={handleAddToBag}
+        >
+          {" "}
+          <GrAddCircle /> Add to Bag{" "}
+        </button>
+      )}
     </div>
   );
 };
