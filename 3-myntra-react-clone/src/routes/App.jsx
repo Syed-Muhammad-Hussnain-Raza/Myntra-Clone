a0ec5604b71541a1f4857f8e116ccb18e3c5f0ef
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-12 22:53:43 +0500

    App.jsx - Updated, fetchStatus introduced here to support proper spinner working with Outlet component

--- 3-myntra-react-clone/src/routes/App.jsx
+++ 3-myntra-react-clone/src/routes/App.jsx
@@ -2,12 +2,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
+import FetchItems from "../components/FetchItems";
+import { useSelector } from "react-redux";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 function App() {
+  const fetchStatus = useSelector((store) => store.fetchStatus);
+
   return (
     <>
       <Header />
-      <Outlet></Outlet>
+      <FetchItems />
+      {fetchStatus.currentlyFetching ? <LoadingSpinner /> : <Outlet />}
       <Footer />
     </>
   );
