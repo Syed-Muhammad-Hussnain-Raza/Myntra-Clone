33bb08b8b2ec668f54b082037d1c3dffbaea5394
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-13 01:34:27 +0500

    bagSlice.js is introduced in main store

--- 3-myntra-react-clone/src/store/index.js
+++ 3-myntra-react-clone/src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import itemsSlice from "./itemsSlice";
 import fetchStatusSlice from "./fetchStatusSlice";
+import bagSlice from "./bagSlice";
 
 const myntraStore = configureStore({
   reducer: {
     items: itemsSlice.reducer,
     fetchStatus: fetchStatusSlice.reducer,
+    bag: bagSlice.reducer,
   },
 });
 
