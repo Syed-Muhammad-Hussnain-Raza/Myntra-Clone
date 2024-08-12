f6b89985a53aa5944ecf9f89a2e40f65495992d1
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-13 01:29:55 +0500

    Bag.jsx - Show only those items in bag that are added by user to bag from whole items

--- 3-myntra-react-clone/src/routes/Bag.jsx
+++ 3-myntra-react-clone/src/routes/Bag.jsx
@@ -1,28 +1,23 @@
 import BagItem from "../components/BagItem";
 import BagSummary from "../components/BagSummary";
+import { useSelector } from "react-redux";
 
 const Bag = () => {
-  const item = {
-    id: "001",
-    image: "images/1.jpg",
-    company: "Carlton London",
-    item_name: "Rhodium-Plated CZ Floral Studs",
-    original_price: 1045,
-    current_price: 606,
-    discount_percentage: 42,
-    return_period: 14,
-    delivery_date: "10 Oct 2023",
-    rating: {
-      stars: 4.5,
-      count: 1400,
-    },
-  };
+  const bagItems = useSelector((state) => state.bag);
+  const items = useSelector((state) => state.items);
+
+  const finalItems = items.filter((item) => {
+    const itemIndex = bagItems.indexOf(item.id);
+    return itemIndex >= 0;
+  });
 
   return (
     <main>
       <div className="bag-page">
         <div className="bag-items-container">
-          <BagItem item={item}/>
+          {finalItems.map((item) => (
+            <BagItem key={item.id} item={item} />
+          ))}
         </div>
         <BagSummary />
       </div>
