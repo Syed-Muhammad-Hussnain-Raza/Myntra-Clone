f0603afa2040a310885e3f29017cd00ffeee9b37
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-12 16:49:35 +0500

    BagSummary and BagItem components are imported and An item object also created here so that it can be passed to BagItem

--- 3-myntra-react-clone/src/routes/Bag.jsx
+++ 3-myntra-react-clone/src/routes/Bag.jsx
@@ -1,9 +1,30 @@
+import BagItem from "../components/BagItem";
+import BagSummary from "../components/BagSummary";
+
 const Bag = () => {
+  const item = {
+    id: "001",
+    image: "images/1.jpg",
+    company: "Carlton London",
+    item_name: "Rhodium-Plated CZ Floral Studs",
+    original_price: 1045,
+    current_price: 606,
+    discount_percentage: 42,
+    return_period: 14,
+    delivery_date: "10 Oct 2023",
+    rating: {
+      stars: 4.5,
+      count: 1400,
+    },
+  };
+
   return (
     <main>
       <div className="bag-page">
-        <div className="bag-items-container"></div>
-        <div className="bag-summary"></div>
+        <div className="bag-items-container">
+          <BagItem item={item}/>
+        </div>
+        <BagSummary />
       </div>
     </main>
   );
