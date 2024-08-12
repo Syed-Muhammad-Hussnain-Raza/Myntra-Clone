6140b28569386fb8390c57f9c6e9fe8775d856d6
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-12 22:49:14 +0500

    Home.jsx - Updated, now use store items and map on them

--- 3-myntra-react-clone/src/routes/Home.jsx
+++ 3-myntra-react-clone/src/routes/Home.jsx
@@ -1,26 +1,15 @@
+import { useSelector } from "react-redux";
 import HomeItem from "../components/HomeItem";
 
-const item = {
-  id: "001",
-  image: "images/1.jpg",
-  company: "Carlton London",
-  item_name: "Rhodium-Plated CZ Floral Studs",
-  original_price: 1045,
-  current_price: 606,
-  discount_percentage: 42,
-  return_period: 14,
-  delivery_date: "10 Oct 2023",
-  rating: {
-    stars: 4.5,
-    count: 1400,
-  },
-};
-
 const Home = () => {
+  const items = useSelector((store) => store.items);
+
   return (
     <main>
       <div className="items-container">
-        <HomeItem item={item} />
+        {items.map((item) => (
+          <HomeItem key={item.id} item={item} />
+        ))}
       </div>
     </main>
   );
