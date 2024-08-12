64aac5cae006ff0fcf66153d29f4880536b1837f
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-12 22:39:24 +0500

    Store provider is used here so that each component of my project will have access to store

--- 3-myntra-react-clone/src/main.jsx
+++ 3-myntra-react-clone/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./routes/App.jsx";
-import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import myntraStore from "./store/index.js";
+
+import App from "./routes/App.jsx";
 import Bag from "./routes/Bag.jsx";
 import Home from "./routes/Home.jsx";
+import "./index.css";
 
 // Defining router
 const router = createBrowserRouter([
@@ -23,6 +26,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={myntraStore}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
